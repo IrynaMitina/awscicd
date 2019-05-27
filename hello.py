eb0d63292500bcdc40c6a7fa64b55fcd921096f3
Mitina Irina <pui@list.ru>
2019-05-27 15:58:11 +0300

    fib service

--- hello.py
+++ hello.py
@@ -1,21 +1,41 @@
-from flask import request
-from flask import Flask
-app = Flask(__name__)
+import time
+import json
+import tornado.ioloop
+import tornado.web
+import random
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
-@app.route('/g<username>')
-def hello_g(username=None):
-    print("username={}, {!r}".format(username, request))
-    print("id={}".format(request.args.get('id', None)))	
-    print("fid={}".format(request.args.get('fid', None)))	
-    #print("username={}, keyword={}, id={}, headers[0]={}".format(
-    #	username, request.args['keyword'], request.args['id'], request.headers[0])
-    #)
-    return "fixed"
+def fibonacci_num(snum):
+    if snum in (0, 1):
+        return 1
+    else:
+        return fibonacci_num(snum - 1) + fibonacci_num(snum - 2)
+
+
+class FibonacciHandler(tornado.web.RequestHandler):
+    def get(self):
+        snum = random.randint(1, 10)
+        print("got request, snum={}".format(snum))
+        resp = {
+            'date': time.ctime(),
+            'label': 'FIBONACCI',
+            'sequence num': snum,
+            'fibonacci': fibonacci_num(snum)
+        }
+        self.write(json.dumps(resp))
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hallo everyone! This is Fibonacci numbers random generator service. "
+                   "To check it, visit {}fib".format(self.request.uri))
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app = tornado.web.Application([
+        (r"/fib", FibonacciHandler),
+        (r"/", MainHandler),
+    ])
+    app.listen(5555)
+    print("Tornado started on port 5555")
+    tornado.ioloop.IOLoop.current().start()
